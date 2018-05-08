f86b8b6b855ad90dabfeed18f899e4e9aa8b0b3a
Vadim Belman <vrurg@lflat.org>
2018-05-07 22:25:34 -0400

    Minor: version bump

--- lib/Pod/Weaver/Plugin/Include.pm
+++ lib/Pod/Weaver/Plugin/Include.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Pod::Weaver::Plugin::Include;
 
-our $VERSION = 'v0.1.8';
+our $VERSION = 'v0.1.9';
 
 # ABSTRACT: Support for including sections of Pod from other files
 
--- lib/Pod/Weaver/Plugin/Include/Finder.pm
+++ lib/Pod/Weaver/Plugin/Include/Finder.pm
@@ -1,7 +1,7 @@
 #
 package Pod::Weaver::Plugin::Include::Finder;
 
-our $VERSION = 'v0.1.8';
+our $VERSION = 'v0.1.9';
 
 # ABSTRACT: Finds source Pods in .pod files or modules.
 
